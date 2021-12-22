337ffd03acaabd2e3f36fc4ca141419b92c08b1c
chalie <formathead79@gmail.com>
2021-12-22 18:49:05 +0900

    init

--- RxSwift Study/ViewController.swift
+++ RxSwift Study/ViewController.swift
@@ -1,19 +1,24 @@
 //
 //  ViewController.swift
 //  RxSwift Study
-//
 //  Created by chalie on 2021/11/26.
 //
 
+
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
-}
+}//End Of The Class
+
 
