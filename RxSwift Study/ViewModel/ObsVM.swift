8603b91641cb7ae4540f3a4a995cb8e290b29239
chalie <formathead79@gmail.com>
2021-12-23 18:46:05 +0900

    add two Txtfld

---
+++ RxSwift Study/ViewModel/ObsVM.swift
@@ -0,0 +1,21 @@
+//
+//  ObsVM.swift
+//  RxSwift Study
+//
+//  Created by chalie on 2021/12/23.
+//
+
+import Foundation
+import RxSwift
+
+class ObsVM {
+    let streamNo: Observable<Int>
+    let streamEvent: ObsEvent<Int>
+    
+    init(streamNo: Observable<Int>, streamEvent: Observable<Int>) {
+        
+    }
+    
+    
+}//End Of The Class
+
